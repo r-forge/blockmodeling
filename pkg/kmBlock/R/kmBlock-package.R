f5771fb9cfe113f8d600f1c4ae287a6e061e6d16
aleszib (Aleš Žiberna) <aleszib@gmail.com>
2025-07-24 13:52:43 +0000

    kmBlock: Added the source file for the package help file (roxygen).
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/blockmodeling@427 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/kmBlock/R/kmBlock-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+#' @inherit kmBlockORPC references examples
+#' @seealso \code{\link{kmBlockORPC}}
+#' @seealso \code{\link{kmBlockC}}
+#'
+"_PACKAGE"
+## usethis namespace: start
+## usethis namespace: end
+NULL
